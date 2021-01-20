dfb17767d24d2a976eae09253ea5e5eaf1ca17a4
Damy Metzke <damy.metzke@gmail.com>
2021-01-20 12:01:12 +0100

    Install and init jest.

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: "./test",
+
+  testRegex: "[^]+\\.ts",
+};
