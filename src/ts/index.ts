07629ae3076172fd5a33b6fe7ccc68a6c582f061
Damy Metzke <damy.metzke@gmail.com>
2021-01-20 09:58:25 +0100

    Implement service worker.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,28 @@
 import "../style/style.scss";
 
-console.log("Hello World!");
+const JS_NAME = document
+  .querySelector("head script")
+  .getAttribute("src")
+  .slice(1);
+const CSS_NAME = document
+  .querySelector("head link")
+  .getAttribute("href")
+  .slice(1);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register(`/service-worker.js${""}`, { scope: "/" })
+      .then(
+        function (registration) {
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function (err) {
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
+  });
+}
