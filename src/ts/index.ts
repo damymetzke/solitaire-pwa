09a1c774c020adab6c33159f0e2795a3a8b0aebc
Damy Metzke <damy.metzke@gmail.com>
2021-01-20 11:36:25 +0100

    Fix lint errors.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,5 @@
 import "../style/style.scss";
 
-const JS_NAME = document
-  .querySelector("head script")
-  .getAttribute("src")
-  .slice(1);
-const CSS_NAME = document
-  .querySelector("head link")
-  .getAttribute("href")
-  .slice(1);
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
