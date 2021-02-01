bcaed86aa6c82eeb778fd762773f5c39bef2c9d2
Damy Metzke <damy.metzke@gmail.com>
2021-02-01 11:41:57 +0100

    Fix postMessage type.
    Typescript did not recognize the correct type.

--- src/ts/future/futureWorker.ts
+++ src/ts/future/futureWorker.ts
@@ -1,8 +1,12 @@
 import * as solitaire from "../solitaire/solitaire";
 
+// todo: use a better suited type
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const postMessageToMain = <(data: any) => void>postMessage;
+
 async function run() {
   await solitaire.waitUntilLoaded();
-  postMessage({ type: "loaded" });
+  postMessageToMain({ type: "loaded" });
 }
 
 run();
