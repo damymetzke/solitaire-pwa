53e40f7e5e9832a152c36bcdfd8b62e31c576152
Damy Metzke <damy.metzke@gmail.com>
2021-02-13 16:52:17 +0100

    Start implementing drag and drop.

--- src/ts/stack/stackHandler.ts
+++ src/ts/stack/stackHandler.ts
@@ -13,8 +13,9 @@ interface MoveCommand {
 }
 
 export default class StackHandler {
-  moveCommands: MoveCommand[];
-  collection: CardCollection;
+  moveCommands: MoveCommand[] = [];
+  collection: CardCollection = null;
+  dirty: DrawState = DrawState.NONE;
 
   constructor(numCards: number[], cardBackImage: HTMLImageElement) {
     this.collection = new CardCollection(numCards, cardBackImage);
@@ -28,8 +29,36 @@ export default class StackHandler {
   getHome(stack: number, index: number): [number, number] {
     return [0, 0];
   }
-  // moveToHome(stack: number, index: number): void {}
-  // setToHome(stack: number, index: number): void {}
+  moveToHome(stack: number, index: number): void {
+    if (!this.collection.cardExists(stack, index)) {
+      console.warn(`Attempt to move non existing card [${stack},${index}].`);
+    }
+
+    const card = this.collection.getCard(stack, index);
+    const [homeX, homeY] = this.getHome(stack, index);
+
+    this.moveCommands.push({
+      startX: card.left,
+      startY: card.top,
+      endX: homeX,
+      endY: homeY,
+      length: Math.sqrt((homeX - card.left) ** 2 + (homeY - card.top) ** 2),
+      card: card,
+      progress: 0,
+    });
+    this.dirty = DrawState.MOVE_ONLY;
+  }
+  setToHome(stack: number, index: number): void {
+    if (!this.collection.cardExists(stack, index)) {
+      console.warn(`Attempt to set non existing card [${stack},${index}]`);
+      return;
+    }
+
+    const card = this.collection.getCard(stack, index);
+    const [homeX, homeY] = this.getHome(stack, index);
+    card.left = homeX;
+    card.top = homeY;
+  }
   resetAll(): void {
     this.collection.forEachCard((card, _index, stackIndex, cardIndex) => {
       const [homeX, homeY] = this.getHome(stackIndex, cardIndex);
@@ -40,15 +69,30 @@ export default class StackHandler {
 
   drawUpdate(delta: number): DrawState {
     if (this.moveCommands.length == 0) {
-      return DrawState.NONE;
+      const result = this.dirty;
+      this.dirty = DrawState.NONE;
+      return result;
     }
-    this.moveCommands.forEach((command) => {
-      command.progress += delta / command.length;
+    this.moveCommands.filter((command) => {
+      command.progress = Math.min(
+        command.progress + delta / command.length / 5000,
+        1
+      );
       command.card.top =
         command.startY + command.progress * (command.endY - command.startY);
       command.card.left =
         command.startX + command.progress * (command.endX - command.startX);
+      if (command.progress < 1) {
+        return true;
+      }
+
+      command.card.isMoving = false;
+      this.dirty = DrawState.ALL;
+
+      return false;
     });
-    return DrawState.MOVE_ONLY;
+    const result = Math.max(DrawState.MOVE_ONLY, this.dirty);
+    this.dirty = DrawState.NONE;
+    return result;
   }
 }
