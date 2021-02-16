263dfec58127e8871ebbda7b64cb67f80fb3c2d9
Damy Metzke <damy.metzke@gmail.com>
2021-02-16 11:28:32 +0100

    Clean MoveNotationDecoder test file.

--- test/unit/moveNotationDecoder.test.ts
+++ test/unit/moveNotationDecoder.test.ts
@@ -1,52 +1,57 @@
 import MoveNotationDecoder from "../../src/ts/moveNotation/moveNotationDecoder";
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toResolveToMoves: (
+        [sourceStack, sourceCard]: [number, number],
+        target: number,
+        modifiers: [boolean, boolean, boolean]
+      ) => CustomMatcherResult;
+    }
+  }
+}
+expect.extend({
+  toResolveToMoves(
+    recieved: unknown,
+    [sourceStack, sourceCard]: [number, number],
+    target: number,
+    modifiers: [boolean, boolean, boolean]
+  ): { pass: boolean; message: () => string } {
+    if (typeof recieved !== "string") {
+      return {
+        pass: false,
+        message: () => "Expected a string",
+      };
+    }
+
+    const decoder = new MoveNotationDecoder(recieved, null);
+    const expectedObject = {
+      modifiers: {
+        turnToFront: modifiers[0],
+        allowToMove: modifiers[1],
+        doNotAllowToMove: modifiers[2],
+      },
+      sourceStack: sourceStack,
+      sourceCard: sourceCard,
+      target: target,
+    };
+
+    if (this.isNot) {
+      expect(decoder.moves).not.toStrictEqual([[expectedObject]]);
+    } else {
+      expect(decoder.moves).toStrictEqual([[expectedObject]]);
+    }
+
+    return {
+      pass: !this.isNot,
+      message: () => "",
+    };
+  },
+});
 
 test("Move notation decoder can handle modifiers, sources and targets", () => {
-  const moveOnly = new MoveNotationDecoder("0,0:1", null);
-  const modifierOnly = new MoveNotationDecoder("*!1,2", null);
-  const both = new MoveNotationDecoder("!5,3:4", null);
-
-  expect(moveOnly.moves).toStrictEqual([
-    [
-      {
-        modifiers: {
-          turnToFront: false,
-          allowToMove: false,
-          doNotAllowToMove: false,
-        },
-        sourceStack: 0,
-        sourceCard: 0,
-        target: 1,
-      },
-    ],
-  ]);
-
-  expect(modifierOnly.moves).toStrictEqual([
-    [
-      {
-        modifiers: {
-          turnToFront: true,
-          allowToMove: true,
-          doNotAllowToMove: false,
-        },
-        sourceStack: 1,
-        sourceCard: 2,
-        target: null,
-      },
-    ],
-  ]);
-
-  expect(both.moves).toStrictEqual([
-    [
-      {
-        modifiers: {
-          turnToFront: false,
-          allowToMove: true,
-          doNotAllowToMove: false,
-        },
-        sourceStack: 5,
-        sourceCard: 3,
-        target: 4,
-      },
-    ],
-  ]);
+  expect("0,0:1").toResolveToMoves([0, 0], 1, [false, false, false]);
+  expect("*!1,2").toResolveToMoves([1, 2], null, [true, true, false]);
+  expect("!5,3:4").toResolveToMoves([5, 3], 4, [false, true, false]);
 });
